b9f7a00f2f67c534995b97476472eb6fad1be521
Kunal <kunalsonawane730@gmail.com>
2022-12-21 23:18:14 +0530

    Add: Welcome message for Employee Payroll Service JDBC

---
+++ src/main/java/org/bridgelabz/App.java
@@ -0,0 +1,9 @@
+package org.bridgelabz;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome To Employee Payroll Service JDBC" );
+    }
+}
